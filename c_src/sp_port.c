4c185b8e8f23b57bafe69193a40855b62c486a38
Victor <vicbaz@yandex.ru>
2016-05-25 23:17:29 +0300

    sp

--- c_src/sp_port.c
+++ c_src/sp_port.c
@@ -10,6 +10,9 @@
 
 #include "erlcmd.h"
 
+#define BUFFER_SIZE     512
+#define WRITE_TIMEOUT   500
+
 struct sp
 {
     struct sp_port *port;
@@ -57,13 +60,108 @@ static void sp_init(struct sp *sp, const char *portname)
         sp_free_config(config);
 }
 
+static void init_resp(ei_x_buff *resp, int notif)
+{
+    char hdr[3] = {0, 0, notif != 0};
+
+    if (ei_x_new(resp) < 0)
+        errx(EXIT_FAILURE, "ei_x_new");
+    if (ei_x_append_buf(resp, hdr, sizeof(hdr)) < 0)
+        errx(EXIT_FAILURE, "ei_x_append_buf");
+    if (ei_x_encode_version(resp) < 0)
+        errx(EXIT_FAILURE, "ei_x_encode_version");
+}
+
 static void sp_process(struct sp *sp)
 {
+    ei_x_buff resp;
+    char data[BUFFER_SIZE];
+    int res;
+
+    init_resp(&resp, 1);
+
+    if (ei_x_encode_tuple_header(&resp, 2) < 0)
+        errx(EXIT_FAILURE, "ei_x_encode_tuple_header");
+
+    res = sp_nonblocking_read(sp->port, data, BUFFER_SIZE);
+    if (res < 0) {
+        if (ei_x_encode_atom(&resp, "error") < 0)
+            errx(EXIT_FAILURE, "ei_x_encode_atom");
+        if (ei_x_encode_long(&resp, sp_last_error_code()) < 0)
+            errx(EXIT_FAILURE, "ei_x_encode_long");
+    } else {
+        if (ei_x_encode_atom(&resp, "ok") < 0)
+            errx(EXIT_FAILURE, "ei_x_encode_atom");
+        if (ei_x_encode_binary(&resp, data, res) < 0)
+            errx(EXIT_FAILURE, "ei_x_encode_binary");
+    }
+
+    erlcmd_send(resp.buff, resp.index);
+
+    if (ei_x_free(&resp) < 0)
+        errx(EXIT_FAILURE, "ei_x_free");
 }
 
 static void sp_handle_request(const char *req, void *cookie)
 {
     struct sp *sp = (struct sp *)cookie;
+    int req_index = sizeof(uint16_t);
+    char cmd[MAXATOMLEN];
+    int arity;
+    ei_x_buff resp;
+
+    if (ei_decode_version(req, &req_index, NULL) < 0)
+        errx(EXIT_FAILURE, "message version issue?");
+
+    if (ei_decode_tuple_header(req, &req_index, &arity) < 0 || arity != 2)
+        errx(EXIT_FAILURE, "expecting {cmd, args} tuple");
+
+    if (ei_decode_atom(req, &req_index, cmd) < 0)
+        errx(EXIT_FAILURE, "expecting command atom");
+
+    init_resp(&resp, 0);
+
+    if (strcmp(cmd, "write") == 0) {
+        char data[BUFFER_SIZE];
+        long len, res;
+        int type, size;
+
+        if (ei_decode_binary(req, &req_index, data, &len) < 0)
+            errx(EXIT_FAILURE, "ei_decode_binary");
+
+        if (ei_get_type(req, &req_index, &type, &size) < 0)
+            errx(EXIT_FAILURE, "ei_get_type");
+
+        if (size > BUFFER_SIZE)
+            errx(EXIT_FAILURE, "data too large");
+
+        res = sp_blocking_write(sp->port, data, len, WRITE_TIMEOUT);
+        if (res < 0) {
+            if (ei_x_encode_tuple_header(&resp, 2) < 0)
+                errx(EXIT_FAILURE, "ei_x_encode_tuple_header");
+            if (ei_x_encode_atom(&resp, "error") < 0)
+                errx(EXIT_FAILURE, "ei_x_encode_atom");
+            if (ei_x_encode_long(&resp, sp_last_error_code()) < 0)
+                errx(EXIT_FAILURE, "ei_x_encode_long");
+        } else if (res != len) {
+            if (ei_x_encode_tuple_header(&resp, 2) < 0)
+                errx(EXIT_FAILURE, "ei_x_encode_tuple_header");
+            if (ei_x_encode_atom(&resp, "error") < 0)
+                errx(EXIT_FAILURE, "ei_x_encode_atom");
+            if (ei_x_encode_atom(&resp, "timeout") < 0)
+                errx(EXIT_FAILURE, "ei_x_encode_atom");
+        } else {
+            if (ei_x_encode_atom(&resp, "ok") < 0)
+                errx(EXIT_FAILURE, "ei_x_encode_atom");
+        }
+    } else {
+        errx(EXIT_FAILURE, "unknown command: %s", cmd);
+    }
+
+    erlcmd_send(resp.buff, resp.index);
+
+    if (ei_x_free(&resp) < 0)
+        errx(EXIT_FAILURE, "ei_x_free");
 }
 
 int sp_main(int argc, char *argv[])
@@ -86,7 +184,7 @@ int sp_main(int argc, char *argv[])
         fdset[0].revents = 0;
 
         fdset[1].fd = sp.fd;
-        fdset[1].events = POLLPRI;
+        fdset[1].events = POLLIN;
         fdset[1].revents = 0;
 
         rc = poll(fdset, 2, -1);
@@ -99,7 +197,7 @@ int sp_main(int argc, char *argv[])
         if (fdset[0].revents & (POLLIN | POLLHUP))
             erlcmd_process(&handler);
 
-        if (fdset[1].revents & POLLPRI)
+        if (fdset[1].revents & POLLIN)
             sp_process(&sp);
     }
 
