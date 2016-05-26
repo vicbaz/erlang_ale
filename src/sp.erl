8e6b783ccdd1ba1c16c7cd2c4ecb359b5be988e4
Victor <vicbaz@yandex.ru>
2016-05-26 22:53:42 +0300

    sp

--- src/sp.erl
+++ src/sp.erl
@@ -5,7 +5,9 @@
 %% API
 -export([start_link/2,
          start_link/3,
-         list/1]).
+         stop/1,
+         list/1,
+         write/2]).
 
 %% gen_server
 -export([init/1,
@@ -32,8 +34,14 @@ start_link(SerialPort, Pid) when is_pid(Pid) ->
 start_link(ServerName, SerialPort, Pid) when is_pid(Pid) ->
     gen_server:start_link(ServerName, ?MODULE, [SerialPort, Pid], []).
 
+stop(ServerRef) ->
+    gen_server:call(ServerRef, stop).
+
 list(ServerRef) ->
     gen_server:call(ServerRef, list).
+
+write(ServerRef, Data) when is_binary(Data) ->
+    gen_server:call(ServerRef, {write, Data}).
 
 %% gen_server
 
@@ -41,8 +49,11 @@ init([SerialPort, Pid]) ->
     Port = ale_util:open_port(["sp", SerialPort]),
     {ok, #state{port = Port, listener = Pid}}.
 
-handle_call(list, _From, #state{port = Port} = State) ->
-    Reply = call_port(Port, list),
+handle_call(stop, _From, #state{port = Port} = State) ->
+    port_close(Port),
+    {stop, normal, ok, State#state{port = undefined}};
+handle_call(Command, _From, #state{port = Port} = State) ->
+    Reply = port_call(Port, Command),
     {reply, Reply, State}.
 
 handle_cast(_Request, State) ->
@@ -61,7 +72,7 @@ code_change(_OldVsn, State, _Extra) ->
 
 %% Internal
 
-call_port(Port, Command) ->
+port_call(Port, Command) ->
     Port ! {self(), {command, term_to_binary(Command)}},
     receive
         {Port, {data, <<?REPLY, Response/binary>>}} ->
